b5a564cc90f125298bf60055aa1a676b0d8e273d
YYnfante <yynfante@vimenca.com>
2020-12-22 17:44:21 -0400

    +Changes 5:44

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SignInSide from './Components/SignUp';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SignInSide />
   </React.StrictMode>,
   document.getElementById('root')
 );
